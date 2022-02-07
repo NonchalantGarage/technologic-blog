7b2e74a216a14d9edf0cd2de47d1fb8f53faf3d6
curtis <hong.curtis@gmail.com>
2022-02-07 09:15:14 -0800

    added connection to DB and set imported npm packages

---
+++ server.js
@@ -0,0 +1,42 @@
+// import npm packages
+
+const path = require('path');
+const express = require('express');
+const routes = require('./controllers');
+const exphbs = require('express-handlebars');
+const session = require('express-session');
+
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sequelize = require('./config/connection');
+
+const sess = {
+    secret: 'hush to secret',
+    cookie:{},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+  }
+
+// add middleware 
+app.use(session(sess))
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// turn on routes
+app.use(routes);
+
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+  });
+  
