1b3debfc02387d2731940ee014b1231b897f08c7
curtis <hong.curtis@gmail.com>
2022-02-13 11:04:11 -0800

    added edit post page without using partials so that it's it's own page with a edit post button. Also added front end to include delete button by using the post id from the window.location

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -81,48 +81,44 @@ router.get("/", loggedInOnly, (req, res) => {
 //   });
   // ------------------------------------------------------------------------------------------------------------
 
-//   router.get('/edit/:id', loggedInOnly, (req, res) => {
-//     Post.findByPk(req.params.id, {
-//       attributes: [
-//         'id',
-//         'content',
-//         'title',
-//         'created_at',
-//       ],
-//       include: [
-//         {
-//           model: Comment,
-//           attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-//           include: {
-//             model: User,
-//             attributes: ['username']
-//           }
-//         },
-//         {
-//           model: User,
-//           attributes: ['username']
-//         }
-//       ]
-//     })
-//       .then(postData => {
-//         if (postData) {
-//           const post = postData.get({ plain: true });
-//           res.render('edit-post', {
-//             post,
-//             loggedIn: true
-//           });
-//         } else {
-//           res.status(404).end();
-//         }
-//       })
-//       .catch(err => {
-//         res.status(500).json(err);
-//       });
-//   });
-//   // -----------------------------------------------------------------------------------------------
-//   router.get('/new-post', (req, res) => {
-//     res.render('create-new-post');
-// });
-  
+  router.get('/edit/:id', loggedInOnly, (req, res) => {
+    Post.findByPk(req.params.id, {
+      attributes: [
+        'id',
+        'post_content',
+        'title',
+        'created_at',
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(postData => {
+        if (postData) {
+          const post = postData.get({ plain: true });
+          res.render('edit-post', {
+            post,
+            loggedIn: true
+          });
+        } else {
+          res.status(404).end();
+        }
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+  });
+
 
 module.exports = router;
---
+++ public/javascript/edit-post.js
@@ -0,0 +1,29 @@
+async function editPostHandler(event) {
+  event.preventDefault();
+
+  const title = document.querySelector('input[name="post-title"]').value.trim();
+  const post_content = document
+    .querySelector('input[name="post-content"]')
+    .value.trim();
+
+
+  if (title && post_content) {
+    const response = await fetch("/api/posts", {
+      method: "POST",
+      body: JSON.stringify({
+        title,
+        post_content,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+    if (response.ok) {
+      document.location.replace("/dashboard");
+    } else {
+      alert(response.statusText);
+    }
+  }
+}
+
+document
+  .querySelector(".post-form")
+  .addEventListener("submit", editPostHandler);
